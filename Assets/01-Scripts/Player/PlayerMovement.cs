3d63559d96cb02e468963f484e0d42d80856b14e
prodevy <jonathankodi@gmail.com>
2022-04-16 00:17:13 -0300

    feat: Ajustando level design  - Post processing  - Arvores  - Pedras  - Fog  - Chao

--- Assets/01-Scripts/Player/PlayerMovement.cs
+++ Assets/01-Scripts/Player/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector3 Direction;
     Rigidbody _rigidbody;
+    Collider _collider;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
     }
 
     private void Update()
@@ -20,22 +22,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void SetDirection()
     {
-        Direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
 
     private void FixedUpdate()
     {
-        var currentSpeed = GetSpeed();
+        Move();
+    }
 
-        if (Direction.x > 0 && Direction.z > 0)
-        {
-            currentSpeed /= 2;
-        }
+    private void Move()
+    {
+        if (NextPositionValid())
+            _rigidbody.velocity = Direction.normalized * GetSpeed();
+        else
+            _rigidbody.velocity = Vector3.zero;
+    }
 
-        Debug.Log(PowerupSpeed.GetBonus(Speed));
-        Debug.Log(currentSpeed);
+    private bool NextPositionValid()
+    {
+        Vector3 newPosition = _rigidbody.position + Direction.normalized * Time.fixedDeltaTime * GetSpeed();
 
-        _rigidbody.velocity = Direction * currentSpeed;
+        return Physics.Raycast(newPosition, Vector3.down, _collider.bounds.extents.y);
     }
 
     private float GetSpeed()
